a859c8b575a25c2f813e19b747600b9339a49cad
teamjaf <65031312+teamjaf@users.noreply.github.com>
2021-12-14 08:12:03 +0000

    including js file

---
+++ src/main/java/com/example/WebRTCChat/WebRtcChatApplication.java
@@ -0,0 +1,13 @@
+package com.example.WebRTCChat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebRtcChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebRtcChatApplication.class, args);
+	}
+
+}
